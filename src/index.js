d79a72a38aa660a07859ec7269e471ecbe6bcea8
Aidan Williams <aidanwilliams01@gmail.com>
2023-06-21 15:31:51 -0700

    update UI logic

--- src/index.js
+++ src/index.js
@@ -4,9 +4,26 @@ function onPageLoad() {
   const form = document.querySelector("form");
   function formSubmission(event) {
     event.preventDefault();
-    const title = document.querySelector('input#title');
-    const body = document.querySelector('input#body');
+    const title = document.querySelector('input#title').value;
+    const body = document.querySelector('input#body').value;
     const entry = new Entry(title, body);
+    const h3Element = document.createElement("h3");
+    const pElement = document.createElement("p");
+    const bodyElement = document.querySelector("body");
+    const wordCount = entry.wordCount();
+    const vowelCount = entry.vowelCount();
+    const consonantCount = entry.consonantCount();
+    const teaser = entry.getTeaser();
+    h3Element.append(title);
+    pElement.append(`Word count: ${wordCount}`);
+    pElement.append(document.createElement('br'));
+    pElement.append(`Vowel count: ${vowelCount}`);
+    pElement.append(document.createElement('br'));
+    pElement.append(`Consonant count: ${consonantCount}`);
+    pElement.append(document.createElement('br'));
+    pElement.append(`Teaser: ${teaser}`);
+    bodyElement.append(h3Element);
+    bodyElement.append(pElement);
   }
   form.addEventListener("submit", formSubmission);
 }
--- src/journal.js
+++ src/journal.js
@@ -6,21 +6,21 @@ export default function Entry(title, body) {
 Entry.prototype.wordCount = function() {
   const text = this.body.replaceAll(/[^\w\s]/g, '');
   return text.trim().split(/\s+/).length;
-}
+};
 
 Entry.prototype.vowelCount = function() {
   const body = this.body.toLowerCase();
   return body.match(/[aeiou]/g).length;
-}
+};
 
 Entry.prototype.consonantCount = function() {
   const body = this.body.toLowerCase();
   return body.match(/[bcdfghjklmnpqrstvwxyz]/g).length;
-}
+};
 
 Entry.prototype.getTeaser = function() {
   const periodIndex = this.body.indexOf('.');
-  const firstSentence = this.body.slice(0, periodIndex);
+  const firstSentence = this.body.slice(0, periodIndex + 1);
   const firstSentenceArray =  firstSentence.trim().split(/\s+/);
   let teaser = '';
   for (let index = 0; index < 8; index++) {
@@ -31,4 +31,4 @@ Entry.prototype.getTeaser = function() {
     }
   }
   return teaser.trim();
-}
+};
